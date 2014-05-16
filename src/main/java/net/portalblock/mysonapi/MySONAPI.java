5352d9465fd1b78513940f51410f1ab188020615
portalBlock <portalBlock@fakeEmail.com>
2014-05-16 16:13:36 -0500

    I am not sure what is going on here.....

--- src/main/java/net/portalblock/mysonapi/MySONAPI.java
+++ src/main/java/net/portalblock/mysonapi/MySONAPI.java
@@ -1,11 +1,12 @@
 package net.portalblock.mysonapi;
 
 import net.portalblock.mysonapi.internals.BlankDatabaseException;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.*;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by portalBlock on 4/29/2014.
@@ -21,6 +22,7 @@ public class MySONAPI {
 
     public void start() throws ClassNotFoundException, IllegalAccessException, InstantiationException{
         Class.forName("com.mysql.jdbc.Driver").newInstance();
+
     }
 
     @Deprecated
@@ -44,7 +46,6 @@ public class MySONAPI {
         jsonBuilder.append("{\"row\":[");
         while(set.next()){
             jsonBuilder.append("{ ");
-            int c = 1;
             for(int i = 1; i < count; i++){
                 //Start key JSON
                 jsonBuilder.append(" \"");
@@ -54,9 +55,8 @@ public class MySONAPI {
                 jsonBuilder.append("\"");
                 jsonBuilder.append(set.getString(i));
                 jsonBuilder.append("\"");
-                if(c < count-1){
+                if(i < count){
                     jsonBuilder.append(",");
-                    c++;
                 }
 
             }
@@ -81,8 +81,7 @@ public class MySONAPI {
         jsonBuilder.append("{\"row\":[");
         while(set.next()){
             jsonBuilder.append("{ ");
-            int c = 1;
-            for(int i = 1; i < count; i++){
+            for(int i = 1; i <= count; i++){
                 //Start key JSON
                 jsonBuilder.append(" \"");
                 jsonBuilder.append(data.getColumnLabel(i));
@@ -91,9 +90,8 @@ public class MySONAPI {
                 jsonBuilder.append("\"");
                 jsonBuilder.append(set.getString(i));
                 jsonBuilder.append("\"");
-                if(c < count-1){
+                if(i < count){
                     jsonBuilder.append(",");
-                    c++;
                 }
 
             }
@@ -104,6 +102,53 @@ public class MySONAPI {
             //{ "key1":"value1" , "key2":"value2" },/
         }
         return new JSONObject(jsonBuilder.toString()+"]}").getJSONArray("row");
+    }
+
+    private void addRow(JSONObject rowObject) throws JSONException{
+        Map<String, String> data = new HashMap<>();
+        data = parse(rowObject, data);
+    }
+
+    public void updateRow(String tableName, String key, String value, JSONObject rowData) throws SQLException, JSONException{
+        StringBuilder queryBuilder = new StringBuilder();
+        queryBuilder.append("UPDATE `"+tableName+"` SET ");
+        Map<String, String> data = new HashMap<>();
+        data = parse(rowData, data);
+        int i = 0;
+        for(Map.Entry<String, String> e : data.entrySet()){
+            queryBuilder.append("`"+e.getKey()+"`='"+e.getValue()+"'");
+            if(i < data.size()-1){
+                queryBuilder.append(", ");
+            }
+            i++;
+        }
+        queryBuilder.append(" WHERE `"+key+"`='"+value+"';");
+        System.out.println(queryBuilder.toString());
+        PreparedStatement statement = conn.prepareStatement(queryBuilder.toString());
+        statement.execute();
+    }
+
+    public JSONObject toJSONData(Map<String, String> data){
+        return new JSONObject(data);
+    }
+
+    private Map<String,String> parse(JSONObject json , Map<String,String> out) throws JSONException{
+        Iterator<String> keys = json.keys();
+        while(keys.hasNext()){
+            String key = keys.next();
+            String val = null;
+            try{
+                JSONObject value = json.getJSONObject(key);
+                parse(value,out);
+            }catch(Exception e){
+                val = json.getString(key);
+            }
+
+            if(val != null){
+                out.put(key,val);
+            }
+        }
+        return out;
     }
 
 }
